e50e84ff85f42872508495597805b33aaaf8135b
Ryan Yogan <ryanyogan@gmail.com>
2017-06-12 12:42:35 -0700

    Added basic navigator / back and forth, styles, etc...

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  Navigator,
+} from 'react-native-deprecated-custom-components';
 
-export default class assemblies extends Component {
+import Landing from './app/components/Landing';
+import Dashboard from './app/components/Dashboard';
+import { globals } from './app/styles';
+
+class assemblies extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        style={globals.flex}
+        initialRoute={{ name: 'Ladning' }}
+        renderScene={(route, navigator) => {
+          switch (route.name) {
+            case 'Landing':
+              return <Landing navigator={navigator} />;
+            case 'Dashboard':
+              return <Dashboard navigator={navigator} />;
+            default:
+              return <Landing navigator={navigator} />;
+          }
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('assemblies', () => assemblies);
